19948a559aa2b45a02c92d75d99ccfb84145b874
vaalll <valentinlemenu@hotmail.com>
2016-11-21 21:09:36 +0100

    commit

---
+++ BasicMathLib/BasicMathHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathLib
+{
+    public class BasicMathHelper : Computer.Command
+    {
+        public override double Execute(params string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double[] ConvertToDouble(string[] args)
+        {
+            double[] result = new double[args.Count()];
+
+            try
+            {
+                for (int i = 0; i < args.Count(); i++)
+                {
+                    result[i] = Convert.ToDouble(args[i]);
+                }
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException("One or more of the arguments is not a number");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BasicMathLib/addition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathLib
+{
+    class addition : BasicMathHelper
+    {
+        string name { get { return "addition"; } }
+
+        public override double Execute(params string[] args)
+        {
+            return evaluate(ConvertToDouble(args));
+        }
+
+        private double evaluate(double[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentException("minimum 2 required argument");
+
+            double result = 0;
+
+            foreach (double i in args)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
