53b1f9592c3a53509dd769e1b743f7a7e368b44f
Dizzy <marcin.krasowski@hotmail.fr>
2016-11-22 23:51:57 +0100

    Modified the welcome message

--- SuperCalc/Classes/Interface.cs
+++ SuperCalc/Classes/Interface.cs
@@ -15,7 +15,9 @@ namespace SuperCalc
         //Display a message at start-up
         public void StartMessage()
         {
-            Console.WriteLine("Welcome message");
+            Console.WriteLine("**__-- SuperCalc --__**\n\n" +
+                                "Use \"listall\" to list all the available commands\n" + 
+                                "Or use the commands if you already know them!");
         }
 
         //Controls the display, to be placed in a while(running) loop
